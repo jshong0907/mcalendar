f6b1c6606f9bcaa74b43fb6cd74ab156eaabfbbd
JoonShik <jshong0907@gmail.com>
2022-02-02 02:10:14 +0900

    ADD: get by country id API

--- countries/apis.py
+++ countries/apis.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from ninja import Router
+from django.shortcuts import get_object_or_404
 
 from countries.schemas import CountryOut
 from countries.selectors import filter_countries
@@ -11,4 +12,15 @@ router = Router(tags=['국가'])
 
 @router.get('/', response=List[CountryOut])
 def list_countries(request, continent_name: str = None):
+    """국가 목록 조회 API
+    
+    continent_name을 통하여 필터링한다.
+    continent_name가 없다면 전체 국가 목록을 반환한다.
+    """
     return filter_countries(continent_name=continent_name)
+
+
+@router.get('/{country_id}/', response=CountryOut)
+def get_country(request, country_id: str):
+    """국가 정보 조회 API"""
+    return get_object_or_404(Country, id=country_id)
